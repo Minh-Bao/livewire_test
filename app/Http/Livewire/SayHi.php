352549431afff248b2f4b6f80c5f7a408c21a304
minh-bao <mbnguyen@hotmail.fr>
2021-03-16 18:56:05 +0100

    add components

---
+++ app/Http/Livewire/SayHi.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\User;
+use Livewire\Component;
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Validated;
+
+class SayHi extends Component
+{
+    //NESTING
+    /**
+     * ".... there are a few important caveats to understand about seperating 
+     * components into "parents" and "children" ...."
+     */
+
+    public $contact;
+
+    //EVENTS//////
+    /**
+     * every time an event call refreshChildren is emited fire the refreshMe method
+     *
+     * @var array
+     */
+    protected $listeners = ['refreshChildren' => 'refreshMe'];
+
+    /**
+     * refresh children
+     *
+     * @return void
+     */
+    public function refreshMe(){
+        //
+    }
+
+    public function mount(User $contact){
+        $this->contact = $contact;
+    }
+
+
+    public function render()
+    {
+        return view('livewire.say-hi');
+    }
+}
---
+++ resources/views/livewire/say-hi.blade.php
@@ -0,0 +1,7 @@
+<div>
+
+    Hello {{$contact->name}} : {{now()}}
+
+
+    <button wire:click="$refresh">refresh</button>
+</div>
